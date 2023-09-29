3898dfc8ce881365ae053f2862d209cf1290dd66
nelidgc <69427100+nelidgc@users.noreply.github.com>
2023-09-29 15:58:30 +0700

    Update sedd.py

--- sedd.py
+++ sedd.py
@@ -2,42 +2,34 @@
 
 import json
 import os
- 
+
+
 path_google_chrome = os.path.expanduser(r'~/.config/google-chrome/NativeMessagingHosts')
 browsers = ['chromium', 'yandex-browser', 'chromium-gost']
 upd_dict = {'newAllowFileAccess':True, 'incognito':True}
-username = input("Введите логин пользователя: \n")
 
+  
 os.system(f"""
 cd ~
-wget -P /home/NSO.LOC/{username}/ http://cm.nso.ru:8080/cmj-web/plugin2/cmjproxyplugin2_x64.run
+wget -P ~/ http://cm.nso.ru:8080/cmj-web/plugin2/cmjproxyplugin2_x64.run
 chmod +rwx cmjproxyplugin2_x64.run
 ./cmjproxyplugin2_x64.run
 rm -f cmjproxyplugin2_x64.run
 """)
+try:
+    for browser in browsers:
+        path_browser = os.path.expanduser(f'~/.config/{browser}/')
+        os.system(f'cp -r {path_google_chrome} {path_browser}')
 
-for browser in browsers:
-    path_browser = os.path.expanduser(f'/home/NSO.LOC/{username}/.config/{browser}/')
-    os.system(f'cp -r {path_google_chrome} {path_browser}')
+    for browser in browsers:
+        path_json = os.path.expanduser(f'~/.config/{browser}/Default/Preferences')
+        with open(path_json, 'r') as f:
+            string = json.load(f)
 
-for browser in browsers:
-    path_json = os.path.expanduser(f'/home/NSO.LOC/{username}/.config/{browser}/Default/Preferences')
-    with open(path_json, 'r') as f:
-        string = json.load(f)
-
-    with open(path_json, "w") as f:
-        key = string['extensions']['settings']['dpkefahlefbmfgfgfoppbpkacgdmadpp']
-        key.update(upd_dict)
-        f.write(json.dumps(string))
-    
-    
-    
-
-        
-    
-
-
-
-#[extensions][settings][dpkefahlefbmfgfgfoppbpkacgdmadpp][newAllowFileAccess]
-#[extensions][settings][dpkefahlefbmfgfgfoppbpkacgdmadpp][incognito]
-
+        with open(path_json, "w") as f:
+            key = string['extensions']['settings']['dpkefahlefbmfgfgfoppbpkacgdmadpp']
+            key.update(upd_dict)
+            f.write(json.dumps(string))
+    print("\nВсе прошло успешно!")        
+except FileNotFoundError:
+    print(f"\nЗапустите, потом завершите работу {browser} и повторите запуск скрипта!")
